8ed4253c69d141b672efbea2be2da7094a43a2f8
AbdallahLearn <abdallahjhn1@gmail.com>
2025-01-25 17:22:50 +0300

    editing some stuff on heba branch by Abdullah

--- Controllers/HotelController.cs
+++ Controllers/HotelController.cs
@@ -33,43 +33,38 @@ namespace FinalWebApplication.Controllers
             List<Hotel> hotel = new List<Hotel>
         {
             new Hotel
-            {
-                Id = 1,
-                Name="Hilton Riyadh Olaya",
-                City="Riyadh",
-                Address="https://maps.app.goo.gl/7Up6CZnZmUbq5No47",
-                Description="2.6 km away from Panorama Mall",
-                Image="https://cf.bstatic.com/xdata/images/hotel/square600/587742639.webp?k=050a6a59008123a81233b91e82dce48e4826cf406c28177823f59f6315f44f5c&o=",
-                EventId=1,
+        {
+            Id = 1,
+            Name = "Hilton Riyadh Olaya Luxury Hotel",
+            City = "Riyadh",
+            Address = "https://maps.app.goo.gl/7Up6CZnZmUbq5No47",
+            Description = "2.6 km from Panorama Mall in Riyadh.",
+            Image = "https://cf.bstatic.com/xdata/images/hotel/square600/587742639.webp?k=050a6a59008123a81233b91e82dce48e4826cf406c28177823f59f6315f44f5c&o=",
+            EventId = 1,
+        },
 
-            },
+        new Hotel
+        {
+            Id = 2,
+            Name = "Vittori Palace Hotel and Residences",
+            City = "Riyadh",
+            Address = "https://maps.app.goo.gl/MR6dNRnLaYryWkUe8",
+            Description = "Near Saqr Aljazeera Museum, 4.5 km away.",
+            Image = "https://cf.bstatic.com/xdata/images/hotel/square600/288047810.webp?k=cb83f02a29d491f3f7aede90386f271c131baa1be36053fc845a6a18204ce44b&o=",
+            EventId = 2,
+        },
 
+        new Hotel
+        {
+            Id = 3,
+            Name = "Hyatt Regency Riyadh Olaya",
+            City = "Riyadh",
+            Address = "https://maps.app.goo.gl/m4xrMV1dCk94xAc47",
+            Description = "Located near Tahlia Street in Riyadh.",
+            Image = "https://cf.bstatic.com/xdata/images/hotel/square600/563248959.webp?k=8054089d2dbefaf84faa9341ebb52fb49740ee9f39e03f2f1838ae04b237e579&o=",
+            EventId = 3,
+        },
 
-              new Hotel
-            {
-                Id = 2,
-                Name="Vittori Palace Hotel and Residences",
-                City="Riyadh",
-                Address="https://maps.app.goo.gl/MR6dNRnLaYryWkUe8",
-                Description="Located in Riyadh, 2.8 miles from Saqr Aljazeera Aviation Museum",
-                Image="https://cf.bstatic.com/xdata/images/hotel/square600/288047810.webp?k=cb83f02a29d491f3f7aede90386f271c131baa1be36053fc845a6a18204ce44b&o=",
-                EventId=2,
-
-            },
-
-
-
-                new Hotel
-            {
-                Id = 3,
-                Name="Hyatt Regency Riyadh Olaya",
-                City="Riyadh",
-                Address="https://maps.app.goo.gl/m4xrMV1dCk94xAc47",
-                Description="is located in the heart of the capital, next to the vibrant Tahlia Street and close to commercial and retail districts such as the financial district",
-                Image="https://cf.bstatic.com/xdata/images/hotel/square600/563248959.webp?k=8054089d2dbefaf84faa9341ebb52fb49740ee9f39e03f2f1838ae04b237e579&o=",
-                EventId=3,
-
-            },
 
         };
             return View(hotel);
